402055bf496e1d140390c126284d6c633a927ab5
Tantawiii <flutterdevabudiyab@gmail.com>
2025-03-02 11:57:54 +0200

    create onboarding model add assets

--- lib/core/constant/assets_path.dart
+++ lib/core/constant/assets_path.dart
@@ -4,4 +4,8 @@ class AssetsPATH {
 
   static const String pLogo_splash = "assets/images/Logo_Splash_Screen.png";
 
+  static const String pOnboarding1 = "assets/svgs/Illustration_Onboarding_1.svg";
+  static const String pOnboarding2 = "assets/svgs/Illustration_Onboarding_2.svg";
+  static const String pOnboarding3 = "assets/svgs/Illustration_Onboarding_3.svg";
+
 }
---
+++ lib/featuers/onboarding/view/on_boarding_screen.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:marketi/core/constant/colors_code.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: TColors.CWhite,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
